ccc4dd54d6b388b31033a37de1d6753057bb4ff2
darknessxk <me@darknessxk.com>
2021-04-03 18:43:25 -0400

    Update redux entries

--- src/store/ducks/index.ts
+++ src/store/ducks/index.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
-import exampleReducer from './example.duck';
+import todoReducer from './todo.duck';
 
 export default combineReducers({
-  exampleReducer,
+  todoReducer,
 });
