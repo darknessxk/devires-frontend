e487e17795ab999ba081916076eb25b53c194911
darknessxk <me@darknessxk.com>
2021-04-03 18:42:17 -0400

    Removed old example duck Created todo duck, removed eslint ignore, updated reducers

---
+++ src/store/ducks/todo.duck.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction, Dispatch } from '@reduxjs/toolkit';
+import { RootState } from 'store';
+import { TodoState } from '../../types/TodoState';
+import { TodoItem } from '../../types';
+import { addToList } from '../../services/todo';
+
+const INITIAL_STATE: TodoState = {
+  items: [],
+};
+
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState: INITIAL_STATE,
+  reducers: {
+    addTodo: (state, { payload }: PayloadAction<TodoItem>) => ({
+      ...state,
+      data: payload,
+    }),
+  },
+});
+
+export const { addTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
+
+export const addItem = (item: TodoItem) => async (dispatch: Dispatch) => {
+  const {
+    status, data
+  } = await addToList(item);
+
+  if (status <= 200 && status >= 399) {
+    return; // do some checkings
+  }
+
+  dispatch(addTodo({ data }));
+};
+
+export const selectExampleState = (state: RootState) => state.todoReducer;
