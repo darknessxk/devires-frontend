feffdc7700f6efda3bce0d0da5498ac0dddefd3d
darknessxk <me@darknessxk.com>
2021-04-04 02:48:34 -0400

    Text test

---
+++ src/components/Text/Text.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Text } from './Text';
+
+describe('Text', () => {
+  test('should render', () => {
+    const value = `My random value: ${Math.random()} - and PI! ${Math.PI}`;
+    render(<Text>{value}</Text>);
+
+    expect(screen.findByText(value)).toBeTruthy();
+  });
+});
+
+export default null;
