edb9770a0f9811d24314e3013bec03d21f147142
darknessxk <me@darknessxk.com>
2021-04-04 02:48:15 -0400

    Now sends the object through the post

--- src/services/todo/addToList.ts
+++ src/services/todo/addToList.ts
@@ -2,5 +2,5 @@ import { Instance } from '../client';
 import { TodoItem } from '../../types';
 
 export const addToList = async (item: TodoItem) => {
-  return await Instance.get('/todos');
+  return await Instance.post('/todos', { item });
 };
