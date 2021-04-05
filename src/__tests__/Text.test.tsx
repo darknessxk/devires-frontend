b3aa9406fb9dd53703650c1115a8c97042575867
darknessxk <me@darknessxk.com>
2021-04-05 10:33:09 -0400

    Added internal check for value instead of just checking the callback

--- src/__tests__/Text.test.tsx
+++ src/__tests__/Text.test.tsx
@@ -4,11 +4,12 @@ import '@testing-library/jest-dom/extend-expect';
 import { Text } from '../components';
 
 describe('Text', () => {
-  test('should render', () => {
+  test('should render', async () => {
     const value = `My random value: ${Math.random()} - and PI! ${Math.PI}`;
     render(<Text>{value}</Text>);
+    const item = await screen.findByText(value);
 
-    expect(screen.findByText(value)).toBeTruthy();
+    expect(item.innerText === value).toBeTruthy();
   });
 });
 
