550578f24985af12b130e69a2b34ce4abd80102a
darknessxk <me@darknessxk.com>
2021-04-05 11:20:04 -0400

    Added gradient background to medium screens+

--- src/components/AppBody.ts
+++ src/components/AppBody.ts
@@ -1,3 +1,7 @@
 import tw from 'twin.macro';
 
-export const AppBody = tw.main`font-sans flex justify-center sm:(p-0 m-0) md:(py-56)`;
+export const AppBody = tw.main`
+font-sans flex justify-center sm:(p-0 m-0) md:(py-56)
+w-screen flex-grow
+md:(bg-gradient-to-r from-purple-400 to-indigo-500)
+`;
