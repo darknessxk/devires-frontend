b4de4b24ff0725949b953b4c4e0453f19136a542
darknessxk <me@darknessxk.com>
2021-04-05 11:20:14 -0400

    Removes margin

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,3 +1,3 @@
 import tw from 'twin.macro';
 
-export const Container = tw.div`w-screen h-screen md:h-auto flex flex-col md:(w-80) md:py-2`;
+export const Container = tw.div`w-screen h-screen md:h-auto m-0 flex flex-col md:(w-80)`;
