730c0fcc69563ef238e11030f4383c25a1da3e39
darknessxk <me@darknessxk.com>
2021-04-05 11:21:10 -0400

    Added hover effect to button

--- src/components/RoundedButton.tsx
+++ src/components/RoundedButton.tsx
@@ -1,3 +1,7 @@
 import tw from 'twin.macro';
 
-export const RoundedButton = tw.button`w-10 h-10 border-0 bg-indigo-600 text-gray-200 p-0 m-0 rounded-full mr-4 cursor-pointer`;
+export const RoundedButton = tw.button`
+w-10 h-10 border-0 bg-indigo-600 text-gray-200
+p-0 m-0 rounded-full mr-4 cursor-pointer
+hover:(bg-indigo-500 text-gray-300)
+`;
