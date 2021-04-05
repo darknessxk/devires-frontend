02a8d273349b08f9a6f70532b7f557793c8709cc
darknessxk <me@darknessxk.com>
2021-04-05 05:59:53 -0400

    Enforced done value to be false when creating a new item

--- src/services/todo/addToList.ts
+++ src/services/todo/addToList.ts
@@ -2,5 +2,5 @@ import { Instance } from '../client';
 import { TodoItem } from '../../types';
 
 export const addToList = async (item: TodoItem) => {
-  return await Instance.post('/todos', { item });
+  return await Instance.post('/todos', { ...item, done: false });
 };
