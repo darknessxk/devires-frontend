05cf6a0743a2c81b128c9d8285a4619eb047d815
darknessxk <me@darknessxk.com>
2021-04-05 11:20:44 -0400

    Added vertical padding to all screens instead of only medium+ screens

--- src/components/FlexContainer.tsx
+++ src/components/FlexContainer.tsx
@@ -1,3 +1,3 @@
 import tw from 'twin.macro';
 
-export const FlexContainer = tw.div`w-screen md:(w-80) bg-gray-100 md:py-2 m-0 flex items-center justify-center`;
+export const FlexContainer = tw.div`w-screen md:(w-80) bg-gray-100 m-0 flex items-center justify-center py-4`;
