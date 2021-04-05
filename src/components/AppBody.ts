2add7a83d3b0a6119f67d89b80e8a5701ab5deab
darknessxk <me@darknessxk.com>
2021-04-05 11:04:11 -0400

    Added more mobile stuff

--- src/components/AppBody.ts
+++ src/components/AppBody.ts
@@ -1,3 +1,3 @@
 import tw from 'twin.macro';
 
-export const AppBody = tw.main`font-sans flex justify-center py-56`;
+export const AppBody = tw.main`font-sans flex justify-center sm:(p-0 m-0) md:(py-56)`;
