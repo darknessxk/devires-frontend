a380720c69102524723e21f18d9190505c1ebce4
darknessxk <me@darknessxk.com>
2021-04-05 10:32:27 -0400

    Created input

---
+++ src/components/Input.tsx
@@ -0,0 +1,3 @@
+import tw from 'twin.macro';
+
+export const Input = tw.input`py-2 text-sm text-gray-900 bg-white border-0 border-b-2 rounded-sm pl-1 focus:outline-none focus:bg-white focus:text-gray-900 transition`;
