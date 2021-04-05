be4f74dacab78fd2af8b4dcba42192733867824b
darknessxk <me@darknessxk.com>
2021-04-05 11:04:55 -0400

    Added flex-grow and margin left

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,9 @@
 import tw from 'twin.macro';
 
-export const Input = tw.input`py-2 text-sm text-gray-900 bg-white border-0 border-b-2 rounded-sm pl-1 focus:outline-none focus:bg-white focus:text-gray-900 transition`;
+export const Input = tw.input`
+  py-2 text-sm text-gray-900
+  bg-white border-0 border-b-2
+  rounded-sm pl-1 focus:outline-none
+  focus:bg-white focus:text-gray-900
+  transition flex-grow ml-4
+`;
