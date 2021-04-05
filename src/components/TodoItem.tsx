e391ce99ffed912099d27e89dc75e55aae6526ce
darknessxk <me@darknessxk.com>
2021-04-05 10:30:48 -0400

    Now supports event for deleting

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,15 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
 import tw from 'twin.macro';
 import { ListItem } from './';
 import { TodoItemProps } from '../types';
+import { useAppDispatch } from '../store/hooks';
+import { deleteTodoItem } from '../store/ducks/todo.duck';
 
-const StyledId = tw.span``;
-const StyledName = tw.span``;
+const StyledDescription = tw.span`px-1`;
+const StyledName = tw.span`px-1`;
 
 export const TodoItem: FC<TodoItemProps> = ({ item }) => {
+  const dispatch = useAppDispatch();
+
+  const deleteAction = useCallback(() => {
+    if (item.id) {
+      dispatch(deleteTodoItem(item.id));
+    }
+  }, [item, dispatch]);
+
   return (
     <ListItem>
-      <StyledId>#{item.id}</StyledId> - <StyledName>{item.title}</StyledName>
+      <StyledName>{item.title}</StyledName>
+      <StyledDescription>{item.description}</StyledDescription>
+      <span onClick={() => deleteAction()}>Delete ME</span>
     </ListItem>
   );
 };
