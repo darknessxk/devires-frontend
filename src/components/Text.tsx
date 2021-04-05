458bff22e11ec84905d9d00804a22b50110be162
darknessxk <me@darknessxk.com>
2021-04-05 06:02:32 -0400

    Simplified components pathing

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import tw from 'twin.macro';
 
-const TextStyled = styled.p`
-  ${tw``};
-`;
+const TextStyled = tw.p`text-gray-700 font-thin`;
 
 export const Text: React.FC = ({ children }) => {
   return <TextStyled>{children}</TextStyled>;
