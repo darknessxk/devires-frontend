94541691969c351fa4401473602377abae1972dc
darknessxk <me@darknessxk.com>
2021-04-05 05:59:33 -0400

    Added default db to ignored files, created an example instead

--- src/services/todo/getList.ts
+++ src/services/todo/getList.ts
@@ -1,5 +1,6 @@
 import { Instance } from '../client';
+import { TodoItem } from '../../types';
 
 export const getList = async () => {
-  return await Instance.get('/todos');
+  return await Instance.get<Array<TodoItem>>('/todos');
 };
