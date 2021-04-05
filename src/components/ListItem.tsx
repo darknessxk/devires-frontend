81a5efd01535158be1a6b158f4595508491e9498
darknessxk <me@darknessxk.com>
2021-04-05 11:05:23 -0400

    Flex adjusts and added animation transition

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,3 +1,8 @@
 import tw from 'twin.macro';
 
-export const ListItem = tw.li`p-3 text-center border-b-2 border-red-600`;
+export const ListItem = tw.li`
+p-3 text-center flex place-content-between
+my-1 first:mt-0 last:mb-0 bg-gray-100
+hover:bg-gray-400 transition
+items-center
+`;
