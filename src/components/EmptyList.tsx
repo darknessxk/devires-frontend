85351bd2450cf14c9d21ab26e2bba237dad41d28
darknessxk <me@darknessxk.com>
2021-04-05 11:20:24 -0400

    Centered content using flex hackery

--- src/components/EmptyList.tsx
+++ src/components/EmptyList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import tw from 'twin.macro';
 
-const EmptyListStyled = tw.div``;
+const EmptyListStyled = tw.div`flex-grow content-center items-center justify-center flex`;
 
 export const EmptyList = () => {
   return <EmptyListStyled>Add some new tasks...</EmptyListStyled>;
