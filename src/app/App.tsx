b9a89e91d340e7865a8bdd84f8980b46d009cb32
darknessxk <me@darknessxk.com>
2021-04-05 06:00:08 -0400

    Created the first app lifecycle

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { AppBody, Container, Header, List, ListItem } from '../components';
+import '../styles/main.css';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { getTodoList } from '../store/ducks/todo.duck';
 
 function App() {
+  const todos = useAppSelector((state) => state.todoReducer.items);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getTodoList());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <h1>Hello Code Challenge!</h1>
-    </div>
+    <AppBody className="App">
+      <Container>
+        <Header>My Things</Header>
+        <List>
+          {todos.map((todo) => (
+            <ListItem item={todo} key={todo.id} />
+          ))}
+        </List>
+      </Container>
+    </AppBody>
   );
 }
 
